e759fd85b5bcd0e88a1f243b8246255bdec8d943
Tom Engan <post@neoweb.no>
2022-09-19 14:13:02 +0200

    Add Hotels API Endpoint

---
+++ HotelListing.API/Contracts/IHotelsRepository.cs
@@ -0,0 +1,10 @@
+﻿using HotelListing.API.Data;
+using System.Threading.Tasks;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IHotelsRepository : IGenericRepository<Hotel>
+    {
+        // Task<Hotel> GetDetails(int id);
+    }
+}
